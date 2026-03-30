1e9e45249d9cc0fa986c0ae1c9fe5e751f2d520a
Tantand24 <tristan.dang@email.com>
2026-03-30 12:11:57 -0600

    updated player detail and team detail

--- app/src/main/java/com/example/goatsoccerapp/ui/screens/PlayerDetailScreen.kt
+++ app/src/main/java/com/example/goatsoccerapp/ui/screens/PlayerDetailScreen.kt
@@ -1,44 +1,63 @@
 package com.example.goatsoccerapp.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.goatsoccerapp.ui.theme.GOATSoccerAPPTheme
+import kotlinx.serialization.EncodeDefault
 
 //player object needed
 @Composable
 fun PlayerDetailScreen(){
     Column(
-        Modifier.statusBarsPadding()
+        Modifier.fillMaxSize()
+            .statusBarsPadding()
             .padding(10.dp)
     ) {
         Row() {
-            //edit button
-            Button(
-                onClick = {}
+            Text("Player Detail",
+                fontSize = 32.sp,)
+            Row(
+                Modifier.fillMaxWidth(),
+                Arrangement.End
             ) {
-                Text("Edit")
+
+                //edit button
+                Button(
+                    onClick = {}
+                ) {
+                    Text("Edit")
+                }
+                //delete button
+                Button(
+                    onClick = {}
+                ) {
+                    Text("Delete")
+                }
             }
-            //delete button
-            Button(
-                onClick = {}
-            ) {
-                Text("Delete")
-            }
-            }
-        Row() {
+        }
+        Row(
+            Modifier.fillMaxWidth()
+                .padding(0.dp, 20.dp),) {
             /*player image
             Image(
                 painter = {},
@@ -53,7 +72,9 @@ fun PlayerDetailScreen(){
                 Row() { }
             }
         }
-        Row() {
+        Row(
+            Modifier.fillMaxWidth(),
+            Arrangement.SpaceBetween) {
             Column() {
                 Text("number")
                 Text("Goal")
--- app/src/main/java/com/example/goatsoccerapp/ui/screens/TeamDetailScreen.kt
+++ app/src/main/java/com/example/goatsoccerapp/ui/screens/TeamDetailScreen.kt
@@ -1,18 +1,71 @@
 package com.example.goatsoccerapp.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.goatsoccerapp.ui.theme.GOATSoccerAPPTheme
 
 @Composable
-fun TeamDetailScreen (){
+fun TeamDetailScreen(){
     Column(
         Modifier.statusBarsPadding()
+            .fillMaxSize()
+            .padding(10.dp)
     ) {
-        Row() { }
-        Row() { }
-        Row() { }
+        Row() {
+            //team logo
+            //Image()
+            Column() {
+                Text("Team Name")
+                Text("Coach: ")
+            }
+        }
+        Row(
+            Modifier.fillMaxWidth(),
+            Arrangement.SpaceBetween) {
+            Column() {
+                Text("number")
+                Text("Wins")
+            }
+            Column() {
+                Text("number")
+                Text("Draws")
+            }
+            Column() {
+                Text("number")
+                Text("Loses")
+            }
+            Column() {
+                Text("number")
+                Text("Point")
+            }
+        }
+        Row(Modifier.fillMaxWidth(),
+            Arrangement.SpaceBetween) {
+            Text("Roster")
+            Button(
+                onClick = {}
+            ) { }
+        }
+        Column() { }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TeamDetailPreview() {
+    GOATSoccerAPPTheme {
+        TeamDetailScreen()
     }
 }
